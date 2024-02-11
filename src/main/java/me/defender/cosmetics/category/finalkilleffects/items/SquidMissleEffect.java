4d535224d65ed07bc6dadf4fcf43d3f9782e115a
IamTheDefender <72926871+IamTheDefender@users.noreply.github.com>
2024-02-11 13:39:53 +0530

    Fixed SquidMissleEffect

--- src/main/java/me/defender/cosmetics/category/finalkilleffects/items/SquidMissleEffect.java
+++ src/main/java/me/defender/cosmetics/category/finalkilleffects/items/SquidMissleEffect.java
@@ -117,7 +117,8 @@ public class SquidMissleEffect extends FinalKillEffect {
                     stand.eject();
                     stand.teleport(stand.getLocation().add(0.0, 0.5, 0.0));
                     stand.setPassenger(squid);
-                    stand.getWorld().spigot().playEffect(stand.getLocation(), Effect.FLAME, 0, 0, 0.0f, 0.0f, 0.0f, 0.0f, 1, 128);
+                    Particle particle = new Particle(ParticleType.FLAME, 1, 0.01f, new Vector(0.0f, 0.0f, 0.0f));
+                    HCore.playParticle(victim, stand.getLocation(), particle);
                     victim.playSound(victim.getLocation(), XSound.ENTITY_CHICKEN_EGG.parseSound(), 1.0f, 1.0f);
                     if (this.i1 == 13) {
                         final Firework fw = stand.getWorld().spawn(stand.getLocation(), Firework.class);
