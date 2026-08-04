d7f2dfab6eea340867e5078d3377c129385c78cb
IamTheDefender <72926871+IamTheDefender@users.noreply.github.com>
2026-08-04 21:03:13 +0530

    WIP MBedWars support

--- cosmetic-plugin/src/main/java/xyz/iamthedefender/cosmetics/category/islandtoppers/handler/IslandTopperHandlerSBW.java
+++ cosmetic-plugin/src/main/java/xyz/iamthedefender/cosmetics/category/islandtoppers/handler/IslandTopperHandlerSBW.java
@@ -26,52 +26,50 @@ public class IslandTopperHandlerSBW extends AbstractIslandTopper {
     }
 
     private void spawnTopper(BedwarsGameStartEvent event) {
-        Run.delayed(() -> {
-            event.getGame().getRunningTeams().forEach(runningTeam -> {
-                Player player = null;
-                for (Player p : runningTeam.getConnectedPlayers()) {
-                    if (!CosmeticsPlugin.getInstance().getApi().getSelectedCosmetic(p, CosmeticType.ISLAND_TOPPERS).equals("none")) {
-                        player = p;
+        Run.delayed(() -> event.getGame().getRunningTeams().forEach(runningTeam -> {
+            Player player = null;
+            for (Player p : runningTeam.getConnectedPlayers()) {
+                if (!CosmeticsPlugin.getInstance().getApi().getSelectedCosmetic(p, CosmeticType.ISLAND_TOPPERS).equals("none")) {
+                    player = p;
 
-                        if (new Random().nextDouble() < 0.5)
-                            break;
-                    }
+                    if (new Random().nextDouble() < 0.5)
+                        break;
                 }
+            }
 
-                if (player == null) return;
-                String pathForLocation = "Team." + runningTeam.getName() + ".IslandTopper.";
-                File file = new File(CosmeticsPlugin.getInstance().getDataFolder(), ".data/arenas/" + event.getGame().getName() + ".yml");
+            if (player == null) return;
+            String pathForLocation = "Team." + runningTeam.getName() + ".IslandTopper.";
+            File file = new File(CosmeticsPlugin.getInstance().getDataFolder(), ".data/arenas/" + event.getGame().getName() + ".yml");
 
-                if (!file.exists() || !file.getAbsolutePath().endsWith(".yml")) {
-                    DebugUtil.addMessage("Failed to find the file required for IslandToppers for player " + player.getName() + ": " + file.getAbsolutePath());
-                    return;
-                }
+            if (!file.exists() || !file.getAbsolutePath().endsWith(".yml")) {
+                DebugUtil.addMessage("Failed to find the file required for IslandToppers for player " + player.getName() + ": " + file.getAbsolutePath());
+                return;
+            }
 
-                YamlConfiguration configuration = YamlConfiguration.loadConfiguration(file);
+            YamlConfiguration configuration = YamlConfiguration.loadConfiguration(file);
 
-                String locationString = configuration.getString(pathForLocation + "location");
+            String locationString = configuration.getString(pathForLocation + "location");
 
-                if (locationString == null) {
-                    DebugUtil.addMessage("Failed to find IslandTopper team location for player " + player.getName() + " and team " + runningTeam.getName() + " in " + event.getGame().getName());
-                    return;
-                }
+            if (locationString == null) {
+                DebugUtil.addMessage("Failed to find IslandTopper team location for player " + player.getName() + " and team " + runningTeam.getName() + " in " + event.getGame().getName());
+                return;
+            }
 
-                Location location = null;
+            Location location = null;
 
-                try {
-                    String[] data = locationString.replace("[", "").replace("]", "").split(",");
-                    location = new Location(Bukkit.getWorld(data[5]), Double.parseDouble(data[0]), Double.parseDouble(data[1]), Double.parseDouble(data[2]), Float.parseFloat(data[3]), Float.parseFloat(data[4]));
-                } catch (Exception ignored) {
-                }
+            try {
+                String[] data = locationString.replace("[", "").replace("]", "").split(",");
+                location = new Location(Bukkit.getWorld(data[5]), Double.parseDouble(data[0]), Double.parseDouble(data[1]), Double.parseDouble(data[2]), Float.parseFloat(data[3]), Float.parseFloat(data[4]));
+            } catch (Exception ignored) {
+            }
 
-                if (location == null) {
-                    DebugUtil.addMessage("Invalid or no location found for team " + runningTeam.getName() + ", player " + player.getName() + " in " + event.getGame().getName());
-                    return;
-                }
+            if (location == null) {
+                DebugUtil.addMessage("Invalid or no location found for team " + runningTeam.getName() + ", player " + player.getName() + " in " + event.getGame().getName());
+                return;
+            }
 
-                execute(player, location);
-            });
-        }, 20L);
+            execute(player, location);
+        }), 20L);
     }
 
 }
--- cosmetic-plugin/src/main/java/xyz/iamthedefender/cosmetics/support/bedwars/handler/mBedwars/MBedWarsHandler.java
+++ cosmetic-plugin/src/main/java/xyz/iamthedefender/cosmetics/support/bedwars/handler/mBedwars/MBedWarsHandler.java
@@ -1,15 +1,25 @@
 package xyz.iamthedefender.cosmetics.support.bedwars.handler.mBedwars;
 
+import de.marcely.bedwars.api.BedwarsAPI;
 import de.marcely.bedwars.api.arena.Arena;
+import de.marcely.bedwars.api.arena.Team;
+import de.marcely.bedwars.api.world.WorldStorage;
+import de.marcely.bedwars.api.world.hologram.HologramControllerType;
+import de.marcely.bedwars.api.world.hologram.HologramEntity;
+import de.marcely.bedwars.tools.location.XYZD;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.screamingsandals.bedwars.Main;
+import xyz.iamthedefender.cosmetics.CosmeticsPlugin;
 import xyz.iamthedefender.cosmetics.api.handler.*;
 import xyz.iamthedefender.cosmetics.support.language.LanguageImpl;
 
 import java.io.File;
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 public class MBedWarsHandler implements IHandler {
 
@@ -27,25 +37,55 @@ public class MBedWarsHandler implements IHandler {
 
     @Override
     public ISetupSession getSetupSession(UUID playerUUID) {
+        Player player = Bukkit.getPlayer(playerUUID);
+
+        if (player == null) return null;
+
+        Arena arena = BedwarsAPI.getGameAPI().getArenas().stream().filter(someArena -> someArena.getGameWorldName().equals(player.getWorld().getName())).findFirst().orElse(null);
+
+        if (arena == null) return null;
+
+        File file = new File(CosmeticsPlugin.getInstance().getDataFolder(), ".data/arenas/" + arena.getName() + ".yml");
+
+        if (!file.exists()) {
+            file.getParentFile().mkdirs();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                CosmeticsPlugin.getInstance().getLogger().warning("Failed to create configuration file: " + file.getAbsolutePath());
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+
         return new ISetupSession() {
             @Override
             public UUID getPlayerUUID() {
-                return null;
+                return playerUUID;
             }
 
             @Override
             public FileConfiguration getConfig() {
-                return null;
+                return config;
             }
 
             @Override
             public void saveConfigLoc(String path, Location value) {
-
+                String data = value.getX() + "," + value.getY() + "," + value.getZ() + "," + value.getYaw() + "," + value.getPitch() + "," + value.getWorld().getName();
+                config.set(path, data);
+                saveConfig();
             }
 
             @Override
             public void saveConfig() {
-
+                try {
+                    config.save(file);
+                } catch (IOException e) {
+                    CosmeticsPlugin.getInstance().getLogger().warning("Failed to save configuration file: " + file.getAbsolutePath());
+                    e.printStackTrace();
+                }
             }
         };
     }
@@ -70,40 +110,68 @@ public class MBedWarsHandler implements IHandler {
         return new IArenaUtil() {
             @Override
             public IArenaHandler getArenaByPlayer(Player player) {
+                Arena arena = BedwarsAPI.getGameAPI().getArenaByPlayer(player);
 
+                if (arena == null) return null;
 
                 return new IArenaHandler() {
                     @Override
                     public ITeamHandler getTeam(Player player) {
+                        Team team = arena.getPlayerTeam(player);
+
+                        if (team == null) return null;
+
                         return new ITeamHandler() {
                             @Override
                             public Location getBed() {
-                                return null;
+                                XYZD bedRaw = arena.getBedLocation(team);
+
+                                if (bedRaw == null)
+                                    throw new IllegalArgumentException("Failed to resolve team bed for " + player.getName() + " in arena " + arena.getName());
+
+                                return bedRaw.toLocation(player.getWorld());
                             }
 
                             @Override
                             public List<Player> getPlayers() {
-                                return List.of();
+                                return arena.getPlayersInTeam(team);
                             }
 
                             @Override
                             public String getName() {
-                                return "";
+                                return team.name();
                             }
 
                             @Override
                             public List<Location> getStoreLocations() {
-                                return List.of();
+                                WorldStorage storage = BedwarsAPI.getWorldStorage(arena.getGameWorld());
+                                List<Location> stores = new ArrayList<>();
+
+                                for (HologramEntity hologramEntity : storage.getHolograms()) {
+                                    if (hologramEntity.getControllerType() != HologramControllerType.DEALER && hologramEntity.getControllerType() != HologramControllerType.UPGRADE_DEALER)
+                                        continue;
+
+                                    if (!arena.isInside(hologramEntity.getLocation()))
+                                        continue;
+
+                                    Location loc = hologramEntity.getLocation();
+
+                                    if (loc.distance(getSpawn()) <= 30) {
+                                        stores.add(loc);
+                                    }
+                                }
+
+                                return stores;
                             }
 
                             @Override
                             public Location getSpawn() {
-                                return null;
+                                return Objects.requireNonNull(arena.getTeamSpawn(team)).toLocation(player.getWorld());
                             }
 
                             @Override
                             public int getSize() {
-                                return 0;
+                                return getPlayers().size();
                             }
                         };
                     }
